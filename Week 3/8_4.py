ba1b31f0e8d17cfc5ba33639692c65c8ae88d7ed
Joseph Adams <joseph.adams@blueyonder.co.uk>
2021-03-22 08:32:46 +0000

    some tidyup and comments

--- Week 3/8_4.py
+++ Week 3/8_4.py
@@ -1,19 +1,24 @@
-def encode_word(plaintext):
-    saltPhrase = 'ABC'
+def encode_word(plaintext, saltPhrase):
+    # get the length of the saltphrase
     saltLen = len(saltPhrase)
+    
+    # set up a counter
     i = 0
+    
+    # placeholder for the return string
     returnString = ''   
 
+    # loop through each character in the plaintext
     for x in plaintext:
+
+        # we need to loop the salt, so check if the counter is equal to the salt length
         if i < saltLen:  
-            #plainVal = ord(x)         
-            #saltVal = ord(saltPhrase[i])
-            #saltedVal = plainVal + saltVal
-            #print(plainVal)
-            #print(saltVal)
-            #print(saltedVal)
+            # convert the plantext char to numeric and the current salttext character and sum these
             returnVal = ord(x) + ord(saltPhrase[i])        
+            # convert back to a char
             returnString += chr(returnVal)
+
+            #increment counter
             i += 1
         else:
             i = 0            
@@ -21,6 +26,7 @@ def encode_word(plaintext):
             returnString += chr(returnVal)
     return returnString
 
+# decode word is the same as encode, but subracting values, this could be made as one function with a paremeter
 def decode_word(secretText, saltPhrase):
     saltLen = len(saltPhrase)
     i = 0
@@ -28,12 +34,6 @@ def decode_word(secretText, saltPhrase):
 
     for x in secretText:
         if i < saltLen:  
-            #plainVal = ord(x)         
-            #saltVal = ord(saltPhrase[i])
-            #saltedVal = plainVal + saltVal
-            #print(plainVal)
-            #print(saltVal)
-            #print(saltedVal)
             returnVal = ord(x) - ord(saltPhrase[i])        
             returnString += chr(returnVal)
             i += 1
@@ -43,7 +43,13 @@ def decode_word(secretText, saltPhrase):
             returnString += chr(returnVal)
     return returnString
 
+# these are used a lot so make them vars
 text = 'This is a secret'
-ciper = encode_word(text)
-print("Encrupted text is >>",ciper,"<<")
-print("Plaintext is >>", decode_word(ciper, "ABC"), "<<")
+salt = 'ABC'
+
+# encrypt the text
+ciper = encode_word(text, salt)
+
+# print stuff to the user
+print("Encrypted text is >>",ciper,"<<")
+print("Plaintext is >>", decode_word(ciper, salt), "<<")
