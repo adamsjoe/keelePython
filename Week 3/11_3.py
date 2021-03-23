4ef7b9151ca247fc2b00ceed8c24dd7651352cff
Joseph Adams <joseph.adams@blueyonder.co.uk>
2021-03-23 16:29:46 +0000

    11.3 added

--- Week 3/11_3.py
+++ Week 3/11_3.py
@@ -33,4 +33,49 @@
 
 # Google : John Davies Microsoft : Boris Barry, Humphrey Smith
 
-db = dict()
+db=[
+    {
+        "name":"John",
+        "surname": "Davies",
+        "age": 23,
+        "company": {
+            "name": "Google",
+            "role": "Programmer"
+        }
+    },
+    {
+        "name":"Boris",
+        "surname": "Barry",
+        "age": 27,
+        "company": {
+            "name": "Microsoft",
+            "role": "Analysts"
+            }
+    },
+    {
+        "name":"Humphrey",
+        "surname": "Smith",
+        "age": 22,
+        "company": {
+            "name": "Microsoft",
+            "role": "Trainee programmer"
+            }
+    }
+]
+
+# create a new dictionary
+companies = {}
+for i in db:
+    # extract the name (first and surname)
+    employeeName = i['name'] + ' ' + i['surname']
+    # extract company
+    employeeCompany= i['company']['name']
+
+    # check if the company exists in the dictionary
+    if employeeCompany in companies:
+        # add the employee to the company and add a comma
+        companies[employeeCompany] + ", " + employeeName
+    else:
+        companies[employeeCompany] = employeeName
+    
+print(companies)
