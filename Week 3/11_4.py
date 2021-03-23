140fdff0c050708ca5b6ce23cb4154fb4308f23f
Joseph Adams <joseph.adams@blueyonder.co.uk>
2021-03-23 16:34:26 +0000

    blah

---
+++ Week 3/11_4.py
@@ -0,0 +1,12 @@
+# Dictionaries in Python are Mutable. Dictionaries are passed to functions by reference.
+
+# A dictionary dict is declared as:
+
+# dict={'Andrew':5, 'Brian':3, 'Clive':2, 'David':4}
+# it contains the names of volunteers and the number of times that they have volunteered for a particular duty. The dictionary is regularly updated by providing both the dictionary and a list to a function upd.
+
+# For the current update the following list has been declared - each name represents a new volunteer session:
+
+# ulist = ['Brian', 'David', 'Peter']
+
+# Write and demonstrate the function upd.
