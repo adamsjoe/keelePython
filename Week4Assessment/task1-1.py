1a98f9c00ec6e430f091941a2eb15c19d7bb6bf6
Joseph Adams <joseph.adams@blueyonder.co.uk>
2021-03-25 17:54:31 +0000

    need to do sorting and output next

--- Week4Assessment/task1-1.py
+++ Week4Assessment/task1-1.py
@@ -14,10 +14,12 @@ def openFile(fileIn, skipHeader = False):
     Keyword arguments:
     fileIn -- the file object which will be opened
     skipHeader -- some files have "Headers" in the first row, if this is true we ignore these (defaults to False)
+
+    Returns:
+    data -- the object read from the file.
     """
     # setup a variable to hold the data from the file
     data = ''
-    # myDictionary = {}
     try:
         with open(fileIn, encoding='utf-8-sig',mode='r') as file: # utf-8 with BOM results in \ufeff1 appearing
             reader = csv.reader(file)
@@ -25,9 +27,6 @@ def openFile(fileIn, skipHeader = False):
                 next(reader, None)
             
             data = [tuple(row) for row in reader]
-            # for row in reader:
-            #     key = row[0]
-            #     myDictionary[key] = row[1:]
     except FileNotFoundError: # handle file not found errors with a nice error message
         print('File {} does not exist.'.format(fileIn))
     except: # generic catch all error message
@@ -35,90 +34,121 @@ def openFile(fileIn, skipHeader = False):
     # return the data variable (the file contents)
     return data
 
-def convertEpochToReadable(e):
+def convertEpochToReadable(dateIn):
     """
+    Convert Excel Epoch time to a sting.
+
+    Keyword arguments:
+    dateIn -- the date (in excel epoch) format
+
+    Returns:
+    dateOut -- a date in string format (eg 12-01-1996)
+
+    Note:
     Excel has a strange idea of epoch, as well a bug from Lotus notes, to convert the date to a readable format
-    I used the following code, which was taken from the  stackoverflow post referenced:
+    I used the following code, which was taken from the  Stackover Flow post referenced:
     
     https://stackoverflow.com/questions/14271791/converting-date-formats-python-unusual-date-formats-extract-ymd
     
-    This returns a XXXXXX
     """      
+    # Mac and PC excel have different "start dates" - using a PC, but leaving the mac code in if needed.
     EXCEL_DATE_SYSTEM_PC=1900
-    d = datetime.date(EXCEL_DATE_SYSTEM_PC, 1, 1) + datetime.timedelta(e-2)
-    d = d.strftime("%d-%m-%Y")
-    return d
+    EXCEL_DATE_SYSTEM_MAC=1904
+
+    dateOut = datetime.date(EXCEL_DATE_SYSTEM_PC, 1, 1) + datetime.timedelta(dateIn-2)
+
+    dateOut = dateOut.strftime("%d-%m-%Y")
+
+    return dateOut
 
 def containsDate(stringDate, dateToCheck):
+    """
+    Checks if a string contains another String.
+
+    Keyword arguments:
+    stringDate -- a date in string format, in truth this could be ANY string.
+    dateToCheck -- a part of the date to check, in truth this could be any string.
+
+    Notes:
+    This is essentially a check to see if a substring exists inside a string.  However, being in a function makes it easier to read.
+    """
     if dateToCheck in stringDate:
         return True
     else:
         return False
 
-def max_value(inputlist):
-    return max([sublist[-1] for sublist in inputlist])
-
-def getval(x):
-    global reportDict
-    print(x)
-    return reportDict[x]
-
 def reportContent(loans):
+    """
+    Generate the content for the report.
+
+    Keyword arguments:
+    loans -- the list of data on the loans
+    """
     # reportDict = {}
     for row in loans:        
-        # make things easier to find        
+        # make things easier to reference
         bookNumber = row[0]
-        bookNumber = int(bookNumber)
-        loanedDateEpoch = row[2]
+        bookNumber = int(bookNumber)        
+        
+        # get the loaned date, convert this to an int and convert to a readable string
+        loanedDateEpoch = row[2]                
         loanedDateEpoch = int(loanedDateEpoch)
         LoanedDateReadable = convertEpochToReadable(loanedDateEpoch)
-        
-
-        # we will store the info on the books in a nested dictionary, first ensure book is not already present
+    
+        # We will store the info on the books in a nested dictionary using the booknumber as a key.
+        # First ensure the book is not already present:
         if bookNumber not in reportDict:
             reportDict[bookNumber] = {}
 
+
         if bookNumber in reportDict:
-            # want to check if the date was in 2019, if so add it to a "loaned" count - not done yet
+            # want to check if the date was in 2019, if so add it to a "loaned" count
             if containsDate(LoanedDateReadable, '2019'):
-                # no this is crap
+                # if the book was loanded in 2019, get the number of times this has been out.
                 loanedTimes =  reportDict[bookNumber].get("timesOut2019")
-                if loanedTimes is None: # check for none type cos you suck
+                
+                # However, we may get a None type.  In this occasion, we can assume 
+                # the bookNumber isn't in the dictionary, so set the loanedTimes as 1
+                if loanedTimes is None: 
                     loanedTimes = 1
                 else:
                     loanedTimes += 1
             
+            # Add the times loaned in 2019 to a field in the dictionary
             reportDict[bookNumber]["timesOut2019"] = loanedTimes
+
+            # It seems that some books in the bookloans file don't exist in the books file, so we need to cover this.
+            # We will use the bookFound flag in a search of the books data for the bookNumber used in the loans file.
             bookFound = False
             
             for thing in books:
                 bookRef = thing[0]
-                if bookRef != 'Number':
+                if bookRef != 'Number': # bit sucky, to keep code working I had to bring in the headers.
                     bookRef = int(bookRef)
                 if bookRef == bookNumber:
                     bookFound = True
             if bookFound:
+                # we have found the book - so add the author and title to fields.
                 bookTitle = books[bookNumber][1]
                 bookAuthor = books[bookNumber][2]       
             else:
+                # We cannot disregarad - so we will use some placeholder data.
                 bookTitle = "No Title on file"
                 bookAuthor = "No Author on file"
         else:
             # this is a new book
             reportDict[bookNumber]["timesOut2019"] = 1
 
+        # add to fields
         reportDict[bookNumber]["title"] = bookTitle
-        reportDict[bookNumber]["author"] = bookAuthor
-    # pprint(reportDict)
+        reportDict[bookNumber]["author"] = bookAuthor    
     print(reportDict[1]['timesOut2019'])
     return reportDict
-
 
 
 books = openFile(booksFile, False)
 loans = openFile(bookloansFile, False)
 
-maxbooks = max_value(books)
-temp = reportContent(loans)
+pprint(reportContent(loans))
 
-uniqueSorted = sorted(temp, key = getval) # wtf?!?!?!
+# uniqueSorted = sorted(temp, key = getval) # wtf?!?!?!
