7178b8f646ffaeee7361121ae11730c05b27e57a
Joseph Adams <joseph.adams@blueyonder.co.uk>
2021-03-27 23:26:13 +0000

    Task 1 done

--- Week4Assessment/task1.py
+++ Week4Assessment/task1.py
@@ -1,10 +1,17 @@
+import sys
 import csv
 import datetime
 from pprint import pprint
 
+#reportDict = {}
+
 # csv file names
 bookloansFile = 'bookloans.csv'
 booksFile = 'books.csv'
+reportFileTask1 = 'Task_1_report.csv'
+reportFileTask2 = 'Task_2_report.csv'
+reportFileTask3 = 'Task_3_report.csv'
+
 
 def openFile(fileIn, skipHeader = False):
     """Open a file.
@@ -12,10 +19,12 @@ def openFile(fileIn, skipHeader = False):
     Keyword arguments:
     fileIn -- the file object which will be opened
     skipHeader -- some files have "Headers" in the first row, if this is true we ignore these (defaults to False)
+
+    Returns:
+    data -- the object read from the file.
     """
     # setup a variable to hold the data from the file
     data = ''
-    # myDictionary = {}
     try:
         with open(fileIn, encoding='utf-8-sig',mode='r') as file: # utf-8 with BOM results in \ufeff1 appearing
             reader = csv.reader(file)
@@ -23,73 +32,162 @@ def openFile(fileIn, skipHeader = False):
                 next(reader, None)
             
             data = [tuple(row) for row in reader]
-            # for row in reader:
-            #     key = row[0]
-            #     myDictionary[key] = row[1:]
     except FileNotFoundError: # handle file not found errors with a nice error message
         print('File {} does not exist.'.format(fileIn))
+        sys.exit(1)
     except: # generic catch all error message
         print('Trying to open {} failed.  No further information was available'.format(fileIn))
+        sys.exit(1)
     # return the data variable (the file contents)
     return data
 
-def convertEpochToReadable(e):
+def convertEpochToReadable(dateIn):
     """
+    Convert Excel Epoch time to a sting.
+
+    Keyword arguments:
+    dateIn -- the date (in excel epoch) format
+
+    Returns:
+    dateOut -- a date in string format (eg 12-01-1996)
+
+    Note:
     Excel has a strange idea of epoch, as well a bug from Lotus notes, to convert the date to a readable format
-    I used the following code, which was taken from the  stackoverflow post referenced:
+    I used the following code, which was taken from the  Stackover Flow post referenced:
     
     https://stackoverflow.com/questions/14271791/converting-date-formats-python-unusual-date-formats-extract-ymd
     
-    This returns a XXXXXX
     """      
+    # Mac and PC excel have different "start dates" - using a PC, but leaving the mac code in if needed.
     EXCEL_DATE_SYSTEM_PC=1900
-    d = datetime.date(EXCEL_DATE_SYSTEM_PC, 1, 1) + datetime.timedelta(e-2)
-    d = d.strftime("%d-%m-%Y")
-    return d
+    EXCEL_DATE_SYSTEM_MAC=1904
+
+    dateOut = datetime.date(EXCEL_DATE_SYSTEM_PC, 1, 1) + datetime.timedelta(dateIn-2)
+
+    dateOut = dateOut.strftime("%d-%m-%Y")
+
+    return dateOut
 
 def containsDate(stringDate, dateToCheck):
+    """
+    Checks if a string contains another String.
+
+    Keyword arguments:
+    stringDate -- a date in string format, in truth this could be ANY string.
+    dateToCheck -- a part of the date to check, in truth this could be any string.
+
+    Notes:
+    This is essentially a check to see if a substring exists inside a string.  However, being in a function makes it easier to read.
+    """
     if dateToCheck in stringDate:
         return True
     else:
         return False
 
-def reportContent(loans, books):
+def buildTask1Details(loans):
+    """
+    Generate the content for the report.
+
+    Keyword arguments:
+    loans -- the list of data on the loans
+    """
     reportDict = {}
     for row in loans:        
-        # make things easier to find        
+        # make things easier to reference
         bookNumber = row[0]
-        bookNumber = int(bookNumber)
-        loanedDateEpoch = row[2]
+        bookNumber = int(bookNumber)        
+        
+        # get the loaned date, convert this to an int and convert to a readable string
+        loanedDateEpoch = row[2]                
         loanedDateEpoch = int(loanedDateEpoch)
         LoanedDateReadable = convertEpochToReadable(loanedDateEpoch)
-        
-
-        # we will store the info on the books in a nested dictionary, first ensure it is not already present
+    
+        # We will store the info on the books in a nested dictionary using the booknumber as a key.
+        # First ensure the book is not already present:
         if bookNumber not in reportDict:
             reportDict[bookNumber] = {}
-        
+
         if bookNumber in reportDict:
-            # want to check if the date was in 2019, if so add it to a "loaned" count - not done yet
-            if containsDate(LoanedDateReadable, '2019'):
-                # no this is crap
-                loanedTimes =  reportDict[bookNumber].get("timesOut2019")
-                if loanedTimes is None: # check for none type cos you suck
-                    loanedTimes = 1
-                else:
-                    loanedTimes += 1
+            # we need to check if the book which is being loaned is present in our cut down books.csv
+            bookFound = False
             
-            reportDict[bookNumber]["timesOut2019"] = loanedTimes
+            # loop through the books 
+            for book in books:
+                bookRef = book[0]
+                if bookRef != 'Number': # bit sucky, to keep code working I had to bring in the headers.
+                    bookRef = int(bookRef)
+                if bookRef == bookNumber:
+                    bookFound = True
+            if bookFound:
+                # we have found the book - so add the author and title to fields.
+                bookTitle = books[bookNumber][1]
+                bookAuthor = books[bookNumber][2]       
+                if containsDate(LoanedDateReadable, '2019'):
+                    # if the book was loanded in 2019, get the number of times this has been out.
+                    loanedTimes =  reportDict[bookNumber].get("timesOut2019")
+                    
+                    # However, we may get a None type.  In this occasion, we can assume 
+                    # the bookNumber isn't in the dictionary, so set the loanedTimes as 1
+                    if loanedTimes is None: 
+                        loanedTimes = 1
+                    else:
+                        loanedTimes += 1                                
+            else:
+                # if the book wasn't found, then we blank out the name and author
+                bookTitle = ""
+                bookAuthor = ""
+            
         else:
             # this is a new book
             reportDict[bookNumber]["timesOut2019"] = 1
-        reportDict[bookNumber]["title"] = books[bookNumber-1][1]
-        reportDict[bookNumber]["author"] = books[bookNumber-1][2]
 
+        # add to fields only if the bookTitle is not an empty string
+        if bookTitle != "":
+            # we write this to reportDict
+            reportDict[bookNumber]["timesOut2019"] = loanedTimes
+            reportDict[bookNumber]["title"] = bookTitle
+            reportDict[bookNumber]["author"] = bookAuthor    
+        else:
+            # if the bookTitle is empty, then we also need to remove the booknumber from the final report
+            reportDict.pop(bookNumber)
     pprint(reportDict)
+    return reportDict
 
-books = openFile(booksFile, True)
+def sortAndGenerateTask1Content(dataIn):
+    """
+    For task 1 this converts the dictionary to a list and sorts at the same time.
+
+    Keyword arguments:
+    dataIn --  the dictionary which was built up in the buildTask1Details function.
+    sorts on the field timesOutIn2019 and then converts this to a list for use with the CSV writer import.
+    """
+    reportOut = []
+    print ("{:<12} {:<60} {:<40} {:<10}".format('Book Number','Title','Author','Times Loaned 2019'))
+    for k, v in sorted(dataIn.items(), key=lambda e: e[1]["timesOut2019"]):
+        item = [k, v['title'], v['author'], v['timesOut2019']]
+        reportOut.append(item)
+        print ("{:<12} {:<60} {:<40} {:<10}".format(k, v['title'], v['author'], v['timesOut2019']))
+    return reportOut
+
+def createReport(fileName, headers, content):
+    """
+    Creates a report file (currently a CSV)
+
+    Keyword
+    """
+    try:
+        with open(fileName, 'w', newline="") as outFile:
+            csvwriter = csv.writer(outFile)
+            csvwriter.writerow(headers)    
+            csvwriter.writerows(content)
+    except:
+        print('Trying to create {} failed.  No further information was available'.format(fileName))
+        sys.exit(1) 
+
+# Task 1 specific code
+reportHeaders = ['Book Number','Title','Author','Times Loaned 2019']
+books = openFile(booksFile, False)
 loans = openFile(bookloansFile, False)
-reportContent(loans, books)
-
-#pprint(books)
-#pprint(loans)
+temp = buildTask1Details(loans)
+contents = sortAndGenerateTask1Content(temp)
+createReport(reportFileTask1, reportHeaders, contents)
