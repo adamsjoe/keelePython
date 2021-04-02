ea88897b3207a51e3d387da519d957abc934a596
Joseph Adams <joseph.adams@blueyonder.co.uk>
2021-04-02 14:51:32 +0100

    post linting and config - and task 1 written up

--- Week4Assessment/task1-1.py
+++ Week4Assessment/task1-1.py
@@ -8,7 +8,7 @@ BOOKSFILE = 'books.csv'
 REPORTFILETASK_1 = 'Task_1_report.csv'
 
 
-def open_file(fileIn, skipHeader=False):
+def open_file(file_in, skip_header=False):
     """Open a file and returns the data.
 
         Wraps a basic file open in a function to allow this
@@ -40,34 +40,37 @@ def open_file(fileIn, skipHeader=False):
             fileIn -- the file object whi chwill be opened
             skipHeader -- some files have "Headers" in the
             first row, if this is true we ignore these
-            (defaults to False)
+            (defaults to False) Ultimatley, I never used
+            this parameter
 
         Returns:
-            data -- the object read from the file.
+            data -- a list of tuples
     """
     # setup a variable to hold the data from the file
     data = ''
     try:
-        with open(fileIn, encoding='utf-8-sig', mode='r') as file:
+        with open(file_in, encoding='utf-8-sig', mode='r') as file:
             reader = csv.reader(file)
-            if skipHeader is True:
+            if skip_header is True:
+                # if skip_header is true, then skip to the next line
                 next(reader, None)
-
+            # make each row in the input file into a tuple and add
+            # this to a list to be returned.
             data = [tuple(row) for row in reader]
     # handle file not found errors with a nice error message
     except FileNotFoundError:
-        print('File {} does not exist.'.format(fileIn))
+        print('File {} does not exist.'.format(file_in))
         sys.exit(1)
     except:  # generic catch all error message
         print(
             'Trying to open {} failed.  No further information was available'
-            .format(fileIn))
+            .format(file_in))
         sys.exit(1)
     # return the data variable (the file contents)
     return data
 
 
-def convertEpochToReadable(date_in):
+def convert_epoch_to_readable(date_in):
     """Convert Excel Epoch time to a sting.
 
     This strange epoch format was a challenge for me.  In order to check if
@@ -80,8 +83,6 @@ def convertEpochToReadable(date_in):
     Excel has a strange idea of epoch, as well a bug from Lotus notes,
     to convert the date to a readable format I used the following code,
     which was taken from the  Stackover Flow post referenced:
-
-    https://stackoverflow.com/questions/14271791/converting-date-formats-python-unusual-date-formats-extract-ymd
 
     Before using this function, I tested this by taking the date
     columns in the bookloans.csv and converting them to "date"
@@ -132,7 +133,11 @@ def build_task1_details(loans):
     """Generate the content for the report.
 
     Keyword arguments:
-    loans -- the list of data on the loans
+        loans -- the list of data from the loans CSV file
+
+    Returns:
+        report_dict -- a dictionary (keyed on booknumber) containing
+        times loaned in 2019, the title, the author
     """
     report_dict = {}
     for row in loans:
@@ -140,80 +145,88 @@ def build_task1_details(loans):
         book_number = row[0]
         book_number = int(book_number)
 
-        # get the loaned date, convert this to an int and convert to a
-        # readable string
+        # get the loaned date, convert this to an int and convert
+        # to a readable string
         loaned_date_epoch = row[2]
         loaned_date_epoch = int(loaned_date_epoch)
-        Loaned_date_readable = convertEpochToReadable(loaned_date_epoch)
+        loaned_date_readable = convert_epoch_to_readable(loaned_date_epoch)
 
-        # We will store the info on the books in a nested dictionary
-        # using the booknumber as a key.
-        # First ensure the book is not already present:
+        # We will store the info on the books in a nested dictionary using the
+        # book number as a key.
+        # First ensure the book_number is not already present:
         if book_number not in report_dict:
             report_dict[book_number] = {}
 
         if book_number in report_dict:
-            # want to check if the date was in 2019, if so add it to a
-            # "loaned" count
-            if contains_date(Loaned_date_readable, '2019'):
-                # if the book was loanded in 2019, get the number of
-                # times this has been out.
-                loaned_times = report_dict[book_number].get("timesOut2019")
-
-                # However, we may get a None type.  In this occasion,
-                # we can assume the bookNumber isn't in the dictionary,
-                # so set the loanedTimes as 1
-                if loaned_times is None:
-                    loaned_times = 1
-                else:
-                    loaned_times += 1
-
-            # Add the times loaned in 2019 to a field in the dictionary
-            report_dict[book_number]["timesOut2019"] = loaned_times
-
-            # It seems that some books in the bookloans file don't exist
-            # in the books file, so we need to cover this.  We will use
-            # the bookFound flag in a search of the books data for the
-            # bookNumber used in the loans file.
+            # we need to check if the book which is being loaned is present in
+            # our cut down books.csv
             book_found = False
 
-            for thing in books:
-                book_ref = thing[0]
+            # loop through the books
+            for book in books:
+                book_ref = book[0]
                 # bit sucky, to keep code working I had to bring in the
-                # headers.
+                # headers. This makes sure we don't deal with the headers
                 if book_ref != 'Number':
                     book_ref = int(book_ref)
                 if book_ref == book_number:
                     book_found = True
             if book_found:
-                # we have found the book - so add the author and title
-                # to fields.
+                # we have found the book - so add the author and title to
+                # fields.
                 book_title = books[book_number][1]
                 book_author = books[book_number][2]
+                if contains_date(loaned_date_readable, '2019'):
+                    # if the book was loanded in 2019, get the number of
+                    # times this has been out.
+                    loaned_times = report_dict[book_number].get("timesOut2019")
+
+                    # However, we may get a None type.  In this occasion,
+                    # we can assume the bookNumber isn't in the dictionary,
+                    # so set the loanedTimes as 1
+                    if loaned_times is None:
+                        loaned_times = 1
+                    else:
+                        loaned_times += 1
             else:
-                # We cannot disregarad - so we will use some placeholder data.
-                book_title = "No Title on file"
-                book_author = "No Author on file"
+                # if the book wasn't found, then we blank out the name
+                # and author
+                book_title = ""
+                book_author = ""
+
         else:
             # this is a new book
             report_dict[book_number]["timesOut2019"] = 1
 
-        # add to fields
-        report_dict[book_number]["title"] = book_title
-        report_dict[book_number]["author"] = book_author
-    # print(reportDict[1]['timesOut2019'])
+        # add to fields only if the bookTitle is not an empty string
+        if book_title != "":
+            # we write this to reportDict
+            report_dict[book_number]["timesOut2019"] = loaned_times
+            report_dict[book_number]["title"] = book_title
+            report_dict[book_number]["author"] = book_author
+        else:
+            # if the bookTitle is empty, then we also need to remove the
+            # booknumber from the final report
+            report_dict.pop(book_number)
+
     return report_dict
 
 
 def sort_and_generate_task1_content(data_in):
-    """For task 1 this converts the dictionary to a list and sorts at the
-    same time.
+    """Sorts  the dictionary which is passed in
+    on the field timesOutIn2019 and then converts
+    this to a list for use with the CSV writer function.
+
+    This function also prints to the screen the report to provide
+    the user with some indication that the task in underway.
 
     Keyword arguments:
-    dataIn --  the dictionary which was built up in the buildTask1Details
-    function.
-    sorts on the field timesOutIn2019 and then converts this to a list for
-    use with the CSV writer import.
+        dataIn --  the dictionary which was built up in the
+        buildTask1Details function.
+
+    Returns:
+        report_out -- a listed sorted on timesOutIn2019
+
     """
     report_out = []
     print(
@@ -233,7 +246,10 @@ def sort_and_generate_task1_content(data_in):
 def create_report(file_name, headers, content):
     """Creates a report file (currently a CSV)
 
-    Keyword
+    Keyword arguments:
+        file_name -- the file we wish to create
+        headers -- a list containing the header columns for the report
+        content -- a list with the data to be populated in the file.
     """
     try:
         with open(file_name, 'w', newline="") as out_file:
@@ -243,14 +259,20 @@ def create_report(file_name, headers, content):
     except:
         print(
             'Trying to create {} failed.  No further information was available'
-            .format(file_name))
+            .format(file_name)
+        )
         sys.exit(1)
 
 
 # Task 1 specific code
-reportHeaders = ['Book Number', 'Title', 'Author', 'Times Loaned 2019']
+
+# The headers for our report
+report_headers = ['Book Number', 'Title', 'Author', 'Times Loaned 2019']
+
+# Open the CSV files and assign them to variables
 books = open_file(BOOKSFILE, False)
 loans = open_file(BOOKLOANSFILE, False)
+
 temp = build_task1_details(loans)
 contents = sort_and_generate_task1_content(temp)
-create_report(REPORTFILETASK_1, reportHeaders, contents)
+create_report(REPORTFILETASK_1, report_headers, contents)
