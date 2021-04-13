322851bc0c417228c450e53d3c58225f5f1b3a7a
Joseph Adams <joseph.adams@blueyonder.co.uk>
2021-04-13 16:31:02 +0100

    wip

--- Week 8 Assignment/one.py
+++ Week 8 Assignment/one.py
@@ -1,8 +1,10 @@
 import sys
 import csv
 from os import system, name
+import time
 
 
+# Class definitions
 class LibraryMember(object):
     def __init__(self, id_no, first_name, last_name, gender, email, card_no):
         self._id_no = id_no
@@ -13,18 +15,18 @@ class LibraryMember(object):
         self._card_no = card_no
 
     def printDetails(self):
-        print("Member ID number: ", self._id_no)
-        print("--> Member First Name: ", self._first_name)
-        print("--> Member Last Name : ", self._last_name)
-        print("--> Member Gender    : ", self._gender)
-        print("--> Member Email     : ", self._email)
-        print("--> Member Card No.  : ", self._card_no)
+        print("Details for Card Number"), self._card_no
+        print("--> Member ID number  : ", self._id_no)
+        print("--> Member First Name : ", self._first_name)
+        print("--> Member Last Name  : ", self._last_name)
+        print("--> Member Gender     : ", self._gender)
+        print("--> Member Email      : ", self._email)
 
     def assign_card_no(self, new_card_no):
         self._card_no = new_card_no
 
     def scan(self):
-        return self._id_no
+        return self._card_no
 
 
 class LibraryBook(object):
@@ -36,6 +38,14 @@ class LibraryBook(object):
         self._genre = genre
         self._sub_genre = sub_genre
         self._publisher = publisher
+
+    def printDetails(self):
+        print("Details for Book Number"), self._book_number
+        print("--> Book Title       : ", self._title)
+        print("--> Book Author      : ", self._author)
+        print("--> Book Genre       : ", self._genre)
+        print("--> Book Sub Genre   : ", self._sub_genre)
+        print("--> Book Publisher   : ", self._publisher)
 
     def scan(self):
         return self._book_number
@@ -49,6 +59,7 @@ class BookLoan(object):
         self._date_returned = date_returned
 
 
+# function definitions
 def open_file(file_in, skip_header=False):
     # setup a variable to hold the data from the file
     data = ''
@@ -78,13 +89,31 @@ def clear():
         _ = system('clear')
 
 
+def get_members_awaiting_cards():
+    # total_members = len(the_members)
+    awaiting_card = 0
+
+    for obj in the_members:
+        if obj._card_no == 0:
+            awaiting_card += 1
+
+    return awaiting_card
+
+
 def main_menu():
+    clear()
+    print("******************************************************************")
+    print("*                                                                *")
+    print("*                      LIBRARY SYSTEM                            *")
+    print("*                                                                *")
+    print("******************************************************************")
     strs = ('Enter 1 for Member Services\n'
             'Enter 2 for Book Loans\n'
             'Enter 3 for Book Returns\n'
             'Enter 4 for Book Reservations\n'
             'Enter 5 to exit : ')
     choice = input(strs)
+    # need to handle non-ints
     return int(choice)
 
 
@@ -95,18 +124,6 @@ def issue_book_loan():
     print("*                        BOOK LOAN                               *")
     print("*                                                                *")
     print("******************************************************************")
-    print()
-    print()
-    print()
-    # need to check here and do a search that the book and member both exist
-    member = input("Please enter a member card number: ")
- 
-    for obj in the_members:
-        if obj._id_no == member:
-            print("member found")
-            obj.printDetails()
-
-    book_id_no = input("Please enter a book number: ")
     return True
 
 
@@ -120,6 +137,48 @@ def book_returns():
     return True
 
 
+def member_details():
+    # clear()
+    print("******************************************************************")
+    print("*                                                                *")
+    print("*                      MEMBER DETAILS                            *")
+    print("*                                                                *")
+    print("******************************************************************")
+    print()
+    print()
+    member = input("Please enter member id number: ")
+
+    for obj in the_members:
+        if obj._id_no == member:
+
+            card_num = ""
+            issue_num = ""
+
+            if obj._card_no == "0":
+                card_num = "No card issued"
+                issue_num = "No card issued"
+            else:
+                card_len = len(obj._card_no)
+                issue_num = obj._card_no[-1]  # this is flaw - can only have 9 cards issues
+                card_num = obj._card_no[0: card_len - 1]
+
+            print("******************************************************************")
+            print("* First Name   : ", obj._first_name.upper())
+            print("* Last Name    : ", obj._last_name.upper())
+            print("* Gender       : ", obj._gender.upper())
+            print("* E-mail       : ", obj._email)
+            print("* Card Number  : ", card_num.upper())
+            print("* Card Issue   : ", issue_num.upper())
+            print("******************************************************************")
+            print()
+            choice2 = input("Return to member services (Y/N)")
+            choice2 = choice2.upper()  # shit
+            while choice2 != 'Y':
+                choice2 = choice2.upper()
+                choice2 = input("Return to member services (Y/N)")
+            return True
+
+
 def member_services():
     clear()
     print("******************************************************************")
@@ -127,7 +186,63 @@ def member_services():
     print("*                     MEMBER SERVICES                            *")
     print("*                                                                *")
     print("******************************************************************")
-    return True
+    print()
+    print()
+    count = get_members_awaiting_cards()
+    print("Number of members awaiting cards: ", count, "\n")
+    strs = ('Enter 1 for Member Details\n'
+            'Enter 2 for Member Application\n'
+            'Enter 3 to return to main menu : ')
+
+    choice = input(strs)
+    if int(choice) == 1:
+        member_details()
+    elif int(choice) == 2:
+        member_application()
+    elif int(choice) == 3:
+        return True
+
+
+def validate_email_address_format(email_in):
+    if '@' in email_in:
+        return True
+    else:
+        return False
+
+
+def create_member(f_name, l_name, gender,e_mail):
+    # get the largest id
+    # call a write function
+    pass
+
+
+def member_application():
+    clear()
+    print("******************************************************************")
+    print("*                                                                *")
+    print("*                    MEMBER APPLICATION                          *")
+    print("*                                                                *")
+    print("******************************************************************")
+    print()
+    print()
+    new_member_first_name = input("Enter first name : ")
+    new_member_last_name = input("Enter last name : ")
+    new_member_gender = input("Enter gender : ")
+    valid_email = False
+    while valid_email is not True:
+        new_member_email = input("Enter email address : ")
+        valid_email = validate_email_address_format(new_member_email)
+
+    # now check the user does not already exist - make this a function
+    for obj in the_members:
+        if obj._first_name.lower() == new_member_first_name.lower() and obj._last_name.lower() == new_member_last_name.lower() and obj._gender.lower() == new_member_gender.lower() and obj._email.lower() == new_member_email.lower():
+            print("USER ALREADY EXISTS")
+            time.sleep(5)
+        else:
+            create_member(new_member_first_name, new_member_last_name, new_member_gender, new_member_email)
+            # create user in the file
+
+    
 
 
 def book_reservations():
@@ -152,23 +267,44 @@ def send_email(addressee, subject, body):
           .format(addressee, subject, body))
 
 
+# main thread
 members = open_file('members.csv', True)
 books = open_file('books.csv', True)
 book_loans = open_file('bookloans.csv')
 
 
+# create a list of objects for library members
 the_members = []
-
 for line in members:
     the_members.append(LibraryMember(
-                                line[0],
-                                line[1],
-                                line[2],
-                                line[3],
-                                line[4],
-                                line[5]
-                               ))
+                            line[0],
+                            line[1],
+                            line[2],
+                            line[3],
+                            line[4],
+                            line[5]
+                            ))
 
+# create a list of objects for books
+the_books = []
+for line in books:
+    the_books.append(LibraryBook(
+                            line[0],
+                            line[2],
+                            line[1],
+                            line[3],
+                            line[4],
+                            line[5]
+                            ))
+
+the_loans = []
+for line in book_loans:
+    the_loans.append(BookLoan(
+                            line[0],
+                            line[1],
+                            line[2],
+                            line[3]
+                            ))
 
 
 # notes to ask
